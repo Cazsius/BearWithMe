a0e91d7b5d402feac5e043868a27cbc1ee4bfef9
MrTrollNugNug <cazawadzki@comcast.net>
2016-11-20 17:42:23 -0500

    Revert "Added a 1.11 Version"
    This reverts commit 6426ca188bd30d4f52e7e495eaa043b3da0df713.

---
+++ src/main/java/com/mrtrollnugnug/bearwithme/CommonProxy.java
@@ -0,0 +1,55 @@
+package com.mrtrollnugnug.bearwithme;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.BiomeDictionary;
+import net.minecraftforge.common.BiomeDictionary.Type;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+
+public class CommonProxy {
+
+	public void preInit() 
+	{
+
+	}
+	public void init() {
+		Collection<Biome> gList = new ArrayList<>();
+		Collections.addAll(gList, BiomeDictionary.getBiomesForType(Type.PLAINS));
+		Collections.addAll(gList, BiomeDictionary.getBiomesForType(Type.FOREST));
+		Collections.addAll(gList, BiomeDictionary.getBiomesForType(Type.HILLS));
+		EntityRegistry.registerModEntity(EntityGrizzlyBear.class, "grizzlybear", 1,
+				BearWithMe.instance, 80, 3, true, new Color(255, 255, 255).getRGB(),
+				new Color(119, 84, 57).getRGB());
+		EntityRegistry.addSpawn(EntityGrizzlyBear.class, 2, 1, 1, EnumCreatureType.MONSTER,
+				gList.toArray(new Biome[0]));
+		
+		
+		Collection<Biome> bList = new ArrayList<>();
+		Collections.addAll(bList, BiomeDictionary.getBiomesForType(Type.CONIFEROUS));
+		Collections.addAll(bList, BiomeDictionary.getBiomesForType(Type.MESA));
+		Collections.addAll(bList, BiomeDictionary.getBiomesForType(Type.SAVANNA));
+		Collections.addAll(bList, BiomeDictionary.getBiomesForType(Type.SPOOKY));
+		
+		EntityRegistry.registerModEntity(EntityBlackBear.class, "blackbear", 2,
+				BearWithMe.instance, 80, 3, true, new Color(255, 255, 255).getRGB(),
+				new Color(0, 0, 0).getRGB());
+		EntityRegistry.addSpawn(EntityBlackBear.class, 2, 1, 1, EnumCreatureType.MONSTER,
+				bList.toArray(new Biome[0]));
+		
+		//sounds
+		
+		
+		
+	}
+
+	public void postInit() 
+	{
+
+	}
+
+}
---
+++ src/main/java/com/mrtrollnugnug/bearwithme/EntityBlackBear.java
@@ -0,0 +1,28 @@
+package com.mrtrollnugnug.bearwithme;
+
+import net.minecraft.entity.EntityAgeable;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.monster.EntityPolarBear;
+import net.minecraft.world.World;
+
+public class EntityBlackBear extends EntityPolarBear
+{
+	public EntityBlackBear(World p_i47154_1_) 
+	{
+		super(p_i47154_1_);
+		this.setSize(1.3F, 1.4F);
+	}
+	public EntityAgeable createChild(EntityAgeable ageable)
+	{
+		return new EntityBlackBear(this.worldObj);
+	}
+    protected void applyEntityAttributes()
+    {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(50.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(25.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.30D);
+        this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(8.0D);
+    }
+    
+}
