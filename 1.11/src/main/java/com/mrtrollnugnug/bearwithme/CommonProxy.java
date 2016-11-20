ffe9180ed8ce9ab63059c5e481c80cbb5b2f8e35
MrTrollNugNug <cazawadzki@comcast.net>
2016-11-20 18:23:18 -0500

    Fixed Entity Register to use MOD_ID Variable

--- 1.11/src/main/java/com/mrtrollnugnug/bearwithme/CommonProxy.java
+++ 1.11/src/main/java/com/mrtrollnugnug/bearwithme/CommonProxy.java
@@ -23,7 +23,7 @@ public class CommonProxy {
 		Collections.addAll(gList, BiomeDictionary.getBiomesForType(Type.PLAINS));
 		Collections.addAll(gList, BiomeDictionary.getBiomesForType(Type.FOREST));
 		Collections.addAll(gList, BiomeDictionary.getBiomesForType(Type.HILLS));
-		EntityRegistry.registerModEntity(new ResourceLocation("bearwithme", "entity/new_grizzly_bear"), EntityGrizzlyBear.class, "grizzlybear", 1, BearWithMe.instance, 80, 3, true, new Color(255, 255, 255).getRGB(),
+		EntityRegistry.registerModEntity(new ResourceLocation(BearWithMe.MOD_ID, "entity/new_grizzly_bear"), EntityGrizzlyBear.class, "grizzlybear", 1, BearWithMe.instance, 80, 3, true, new Color(255, 255, 255).getRGB(),
 				new Color(119, 84, 57).getRGB());
 		EntityRegistry.addSpawn(EntityGrizzlyBear.class, 2, 1, 1, EnumCreatureType.MONSTER,
 				gList.toArray(new Biome[0]));
@@ -35,7 +35,7 @@ public class CommonProxy {
 		Collections.addAll(bList, BiomeDictionary.getBiomesForType(Type.SAVANNA));
 		Collections.addAll(bList, BiomeDictionary.getBiomesForType(Type.SPOOKY));
 		
-		EntityRegistry.registerModEntity(new ResourceLocation("bearwithme", "entity/black_bear"), EntityBlackBear.class, "blackbear", 2,
+		EntityRegistry.registerModEntity(new ResourceLocation(BearWithMe.MOD_ID, "entity/black_bear"), EntityBlackBear.class, "blackbear", 2,
 				BearWithMe.instance, 80, 3, true, new Color(255, 255, 255).getRGB(),
 				new Color(0, 0, 0).getRGB());
 		EntityRegistry.addSpawn(EntityBlackBear.class, 2, 1, 1, EnumCreatureType.MONSTER,
